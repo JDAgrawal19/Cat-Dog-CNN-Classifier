231e00f7eaf1f0040cd4c104dd62d1348c568202
Jitesh Dhoot Agrawal <bansal.jda@gmail.com>
2018-08-21 17:43:18 +0530

    Update cnn.py

--- cnn.py
+++ cnn.py
@@ -52,13 +52,13 @@ train_datagen = ImageDataGenerator(
 test_datagen = ImageDataGenerator(rescale=1./255)
 
 train_set = train_datagen.flow_from_directory(
-        'dataset/training_set',
+        'Cat-Dog-CNN-Classifier/dataset/training_set',
         target_size=(64, 64),
         batch_size=32,
         class_mode='categorical')
 
 test_set = test_datagen.flow_from_directory(
-        'dataset/test_set',
+        'Cat-Dog-CNN-Classifier/dataset/test_set',
         target_size=(64, 64),
         batch_size=32,
         class_mode='categorical')
